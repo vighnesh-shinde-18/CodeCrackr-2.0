0fbe7a6c431f7c24ccfe75430c49d83568245d79
Vighnesh Shinde <160852256+vighnesh-shinde-18@users.noreply.github.com>
2026-02-20 23:41:10 +0530

    Add LANGUAGES constant for programming languages

---
+++ client/src/constants/languages.js
@@ -0,0 +1,15 @@
+const LANGUAGES = [
+    { value: "cpp-gcc-14", label: "C++", monaco: "cpp" },
+    { value: "c-gcc-14", label: "C", monaco: "c" },
+    { value: "python-3.13", label: "Python", monaco: "python" },
+    { value: "java-17", label: "Java", monaco: "java" },
+    { value: "javascript-22", label: "JavaScript", monaco: "javascript" },
+    { value: "typescript-5.6", label: "TypeScript", monaco: "typescript" },
+    { value: "csharp", label: "C#", monaco: "csharp" },
+    { value: "go-1.23", label: "Go", monaco: "go" },
+    { value: "php-8", label:"PHP", monaco: "php" },
+    { value: "ruby", label: "Ruby", monaco: "ruby" },
+    { value: "swift", label: "Swift", monaco: "swift" },
+    { value: "rust-1.85", label: "Rust", monaco: "rust" }
+];
+export {LANGUAGES};
